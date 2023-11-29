c25fd5f554d919973bd7cc6983bbfc95f882e966
“jayaprakash-trika” <jayaprakash.m@trikatechnologies.com>
2023-11-29 16:13:12 +0530

    first commit

---
+++ app/api/route.js
@@ -0,0 +1,8 @@
+import Ably from  "ably/promises"
+
+export async function GET(request){
+    const client=new Ably.Realtime(process.env.ABLY_APY_KEY);
+    const tokenRequestData= await  client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
+    return Response.json(tokenRequestData);
+
+}
